096fc804de70cd0c29a427a1c406df62d977cf16
Rio Mastri <102026713+riomastri@users.noreply.github.com>
2025-02-07 10:06:23 +0700

    Updated/Added simply-static-1-1738897395

---
+++ content/addons/elementor-pro/assets/js/qunit-tests.js
@@ -0,0 +1,12 @@
+/*! elementor-pro - v3.27.0 - 06-02-2025 */
+(()=>{"use strict";var __webpack_modules__=({"../tests/qunit/editor/editor.js":
+/*!***************************************!*\
+  !*** ../tests/qunit/editor/editor.js ***!
+  \***************************************/
+(()=>{})});var __webpack_module_cache__={};function __webpack_require__(moduleId){var cachedModule=__webpack_module_cache__[moduleId];if(cachedModule!==undefined){return cachedModule.exports;}
+var module=__webpack_module_cache__[moduleId]={exports:{}};__webpack_modules__[moduleId](module,module.exports,__webpack_require__);return module.exports;}
+(()=>{
+/*!******************************!*\
+  !*** ../tests/qunit/main.js ***!
+  \******************************/
+var _editor=__webpack_require__(/*! ./editor/editor */"../tests/qunit/editor/editor.js");new _editor.EditorBootstrapPro();})();})();
