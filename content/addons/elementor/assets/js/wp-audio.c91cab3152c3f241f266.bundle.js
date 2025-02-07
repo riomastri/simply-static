e4f46b93487e686d0c6a7a3082934563d029d8dd
Rio Mastri <102026713+riomastri@users.noreply.github.com>
2025-02-07 10:05:21 +0700

    Updated/Added simply-static-1-1738897395

---
+++ content/addons/elementor/assets/js/wp-audio.c91cab3152c3f241f266.bundle.js
@@ -0,0 +1,6 @@
+/*! elementor - v3.27.0 - 03-02-2025 */"use strict";(self["webpackChunkelementorFrontend"]=self["webpackChunkelementorFrontend"]||[]).push([["wp-audio"],{"../assets/dev/js/frontend/handlers/wp-audio.js":
+/*!******************************************************!*\
+  !*** ../assets/dev/js/frontend/handlers/wp-audio.js ***!
+  \******************************************************/
+((__unused_webpack_module,exports)=>{Object.defineProperty(exports,"__esModule",({value:true}));exports["default"]=void 0;class WpAudio extends elementorModules.frontend.handlers.Base{onInit(){super.onInit();window.wp.mediaelement.initialize();}}
+exports["default"]=WpAudio;})}]);
